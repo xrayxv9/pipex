87d32e85eaad70b4726cc13b3116fef1b654af9d
xray <xrayxv.5@gmail.com>
2024-12-11 01:58:42 +0100

    Some new test ideas because more tests = more fun (FALSE)

--- test.c
+++ test.c
@@ -6,7 +6,7 @@
 /*   By: xray <xray@42angouleme.fr>                 +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/10 23:09:30 by xray              #+#    #+#             */
-/*   Updated: 2024/12/11 01:39:49 by xray             ###   ########.fr       */
+/*   Updated: 2024/12/11 01:55:45 by xray             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <unistd.h>
@@ -15,14 +15,6 @@
 
 int main (void)
 {
-    char *path = "/bin/ls";
-
-    char *args[] = {"ls", NULL, NULL};
-
-    if (execv(path, args) == -1) 
-	{
-        perror("exec");
-        return (1);
-    }
-    return (0);
+	char *argv[3] = {"ls", NULL, NULL}; 
+    execv("/bin/ls", argv);
 }
